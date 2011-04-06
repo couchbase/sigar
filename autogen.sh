1f052dc5eec6b8dfce3dd2ebe1b8fa4241353437
Aliaksey Kandratsenka <alk@tut.by>
2011-04-05 17:35:48 -0700

    prefer recent versions of autotools
    Change-Id: I0006ade9a8721227bfcf8fbaee15054f1f529398

--- autogen.sh
+++ autogen.sh
@@ -23,38 +23,38 @@ set -e
 
 ## on macosx glibtoolize, others have libtool
 if test x$LIBTOOLIZE = x; then
-  if test \! "x`which glibtoolize 2> /dev/null | grep -v '^no'`" = x; then
-    LIBTOOLIZE=glibtoolize
+  if test \! "x`which libtoolize 2> /dev/null | grep -v '^no'`" = x; then
+    LIBTOOLIZE=libtoolize
   elif test \! "x`which libtoolize-1.5 2> /dev/null | grep -v '^no'`" = x; then
     LIBTOOLIZE=libtoolize-1.5
-  elif test \! "x`which libtoolize 2> /dev/null | grep -v '^no'`" = x; then
-    LIBTOOLIZE=libtoolize
-  else 
+  elif test \! "x`which glibtoolize 2> /dev/null | grep -v '^no'`" = x; then
+    LIBTOOLIZE=glibtoolize
+  else
     echo "libtoolize 1.5.x wasn't found, exiting"; exit 0
   fi
 fi
 
 ## suse has aclocal and aclocal-1.9
 if test x$ACLOCAL = x; then
-  if test \! "x`which aclocal-1.9 2> /dev/null | grep -v '^no'`" = x; then
-    ACLOCAL=aclocal-1.9
+  if test \! "x`which aclocal 2> /dev/null | grep -v '^no'`" = x; then
+    ACLOCAL=aclocal
   elif test \! "x`which aclocal19 2> /dev/null | grep -v '^no'`" = x; then
     ACLOCAL=aclocal19
-  elif test \! "x`which aclocal 2> /dev/null | grep -v '^no'`" = x; then
-    ACLOCAL=aclocal
-  else 
+  elif test \! "x`which aclocal-1.9 2> /dev/null | grep -v '^no'`" = x; then
+    ACLOCAL=aclocal-1.9
+  else
     echo "automake 1.9.x (aclocal) wasn't found, exiting"; exit 0
   fi
 fi
 
 if test x$AUTOMAKE = x; then
-  if test \! "x`which automake-1.9 2> /dev/null | grep -v '^no'`" = x; then
-    AUTOMAKE=automake-1.9
+  if test \! "x`which automake 2> /dev/null | grep -v '^no'`" = x; then
+    AUTOMAKE=automake
   elif test \! "x`which automake19 2> /dev/null | grep -v '^no'`" = x; then
     AUTOMAKE=automake19
-  elif test \! "x`which automake 2> /dev/null | grep -v '^no'`" = x; then
-    AUTOMAKE=automake
-  else 
+  elif test \! "x`which automake-1.9 2> /dev/null | grep -v '^no'`" = x; then
+    AUTOMAKE=automake-1.9
+  else
     echo "automake 1.9.x wasn't found, exiting"; exit 0
   fi
 fi
@@ -62,25 +62,25 @@ fi
 
 ## macosx has autoconf-2.59 and autoconf-2.60
 if test x$AUTOCONF = x; then
-  if test \! "x`which autoconf-2.59 2> /dev/null | grep -v '^no'`" = x; then
-    AUTOCONF=autoconf-2.59
+  if test \! "x`which autoconf 2> /dev/null | grep -v '^no'`" = x; then
+    AUTOCONF=autoconf
   elif test \! "x`which autoconf259 2> /dev/null | grep -v '^no'`" = x; then
     AUTOCONF=autoconf259
-  elif test \! "x`which autoconf 2> /dev/null | grep -v '^no'`" = x; then
-    AUTOCONF=autoconf
-  else 
+  elif test \! "x`which autoconf-2.59 2> /dev/null | grep -v '^no'`" = x; then
+    AUTOCONF=autoconf-2.59
+  else
     echo "autoconf 2.59+ wasn't found, exiting"; exit 0
   fi
 fi
 
 if test x$AUTOHEADER = x; then
-  if test \! "x`which autoheader-2.59 2> /dev/null | grep -v '^no'`" = x; then
-    AUTOHEADER=autoheader-2.59
+  if test \! "x`which autoheader 2> /dev/null | grep -v '^no'`" = x; then
+    AUTOHEADER=autoheader
   elif test \! "x`which autoheader259 2> /dev/null | grep -v '^no'`" = x; then
     AUTOHEADER=autoheader259
-  elif test \! "x`which autoheader 2> /dev/null | grep -v '^no'`" = x; then
-    AUTOHEADER=autoheader
-  else 
+  elif test \! "x`which autoheader-2.59 2> /dev/null | grep -v '^no'`" = x; then
+    AUTOHEADER=autoheader-2.59
+  else
     echo "autoconf 2.59+ (autoheader) wasn't found, exiting"; exit 0
   fi
 fi

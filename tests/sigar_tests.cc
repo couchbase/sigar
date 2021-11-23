49a1cc744752c1fe3143a125760a7e2602e9366e
Trond Norbye <trond.norbye@gmail.com>
2021-11-23 10:29:36 +0100

    Refactor: Convert unit tests to gtest
    Move the tests over to gtest to make it easier to extend
    and add more tests for all of the features we're actually
    using in sigar
    
    Change-Id: Iaadf5052e5cf2bba891bfdb24e733156ce0f7e8c
    Reviewed-on: https://review.couchbase.org/c/sigar/+/166207
    Reviewed-by: Jim Walker <jim@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ tests/sigar_tests.cc
@@ -0,0 +1,227 @@
+/*
+ *    Copyright 2021-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+/**
+ * Copyright (c) 2009, Sun Microsystems Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of Sun Microsystems Inc. nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <folly/portability/GTest.h>
+#include <sigar.h>
+#include <thread>
+#include <chrono>
+#include <platform/dirutils.h>
+
+class Sigar : public ::testing::Test {
+protected:
+    void SetUp() override {
+        Test::SetUp();
+        ASSERT_EQ(SIGAR_OK, sigar_open(&instance));
+    }
+
+    void TearDown() override {
+        Test::TearDown();
+        sigar_close(instance);
+    }
+
+    void createPidFileAndWait(const std::string& pids) {
+        const auto pidfile = cb::io::mktemp(pids + "/pid_");
+        while (cb::io::isFile(pidfile)) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        exit(EXIT_SUCCESS);
+    }
+
+    sigar_t* instance = nullptr;
+};
+
+TEST_F(Sigar, test_sigar_cpu_get) {
+    sigar_cpu_t cpu;
+    const auto ret = sigar_cpu_get(instance, &cpu);
+    ASSERT_EQ(SIGAR_OK, ret)
+            << "sigar_cpu_get: " << sigar_strerror(instance, ret);
+    EXPECT_NE(SIGAR_FIELD_NOTIMPL, cpu.user);
+    EXPECT_NE(SIGAR_FIELD_NOTIMPL, cpu.sys);
+    EXPECT_NE(SIGAR_FIELD_NOTIMPL, cpu.idle);
+    EXPECT_NE(SIGAR_FIELD_NOTIMPL, cpu.wait);
+    EXPECT_NE(SIGAR_FIELD_NOTIMPL, cpu.total);
+}
+
+TEST_F(Sigar, test_sigar_mem_get) {
+    sigar_mem_t mem;
+    int ret = sigar_mem_get(instance, &mem);
+    ASSERT_EQ(SIGAR_OK, ret)
+            << "sigar_mem_get: " << sigar_strerror(instance, ret);
+
+    EXPECT_LT(0, mem.ram);
+    EXPECT_LT(0, mem.total);
+    EXPECT_LT(0, mem.used);
+    EXPECT_LT(0, mem.free);
+    EXPECT_LT(0, mem.actual_free);
+    EXPECT_LT(0, mem.actual_used);
+}
+
+TEST_F(Sigar, test_sigar_net_connections_get) {
+    sigar_net_connection_list_t connlist;
+    int ret = sigar_net_connection_list_get(
+            instance,
+            &connlist,
+            SIGAR_NETCONN_SERVER | SIGAR_NETCONN_CLIENT | SIGAR_NETCONN_TCP);
+    ASSERT_EQ(SIGAR_OK, ret) << "sigar_net_connection_list_get: "
+                             << sigar_strerror(instance, ret);
+    ASSERT_LE(0, connlist.number);
+
+    for (unsigned long ii = 0; ii < connlist.number; ii++) {
+        sigar_net_connection_t con = connlist.data[ii];
+
+        ASSERT_GT(65536, con.local_port);
+        ASSERT_GT(65536, con.remote_port);
+        ASSERT_LE(0, con.type);
+        ASSERT_LE(0, con.state);
+    }
+
+    ASSERT_EQ(SIGAR_OK, sigar_net_connection_list_destroy(instance, &connlist));
+}
+
+TEST_F(Sigar, test_sigar_pid_get) {
+    const auto pid = sigar_pid_get(instance);
+    ASSERT_NE(0, pid);
+}
+
+TEST_F(Sigar, test_sigar_proc_list_get) {
+    sigar_proc_list_t proclist;
+    int ret = sigar_proc_list_get(instance, &proclist);
+    ASSERT_EQ(SIGAR_OK, ret)
+            << "sigar_proc_list_get: " << sigar_strerror(instance, ret);
+
+    ASSERT_LT(0, proclist.number);
+
+    for (unsigned long ii = 0; ii < proclist.number; ii++) {
+        sigar_pid_t pid = proclist.data[ii];
+        sigar_proc_mem_t proc_mem;
+        sigar_proc_state_t proc_state;
+
+        if (SIGAR_OK == (ret = sigar_proc_mem_get(instance, pid, &proc_mem))) {
+            EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_mem.size);
+            EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_mem.resident);
+#if !(defined(__APPLE__) || defined(WIN32))
+            // MacOS X and win32 do provide them
+            EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_mem.share);
+            EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_mem.minor_faults);
+            EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_mem.major_faults);
+#endif
+#if !defined(__APPLE__)
+            EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_mem.page_faults);
+#endif
+        } else {
+            switch (ret) {
+            case ESRCH:
+            case EPERM:
+                /* track the expected error code */
+                break;
+            default:
+                FAIL() << "sigar_proc_mem_get: errno: " << strerror(errno)
+                       << " " << sigar_strerror(instance, ret);
+            }
+        }
+
+        ret = sigar_proc_state_get(instance, pid, &proc_state);
+        ASSERT_EQ(SIGAR_OK, ret)
+                << "sigar_proc_state_get: " << sigar_strerror(instance, ret);
+        ASSERT_NE(nullptr, proc_state.name);
+#ifndef __APPLE__
+        EXPECT_NE(SIGAR_FIELD_NOTIMPL, proc_state.threads);
+#endif
+    }
+
+    sigar_proc_list_destroy(instance, &proclist);
+}
+
+TEST_F(Sigar, test_sigar_swap_get) {
+    sigar_swap_t swap;
+
+    const auto ret = sigar_swap_get(instance, &swap);
+    ASSERT_EQ(SIGAR_OK, ret)
+            << "sigar_swap_get: " << sigar_strerror(instance, ret);
+    ASSERT_EQ(swap.total, swap.used + swap.free);
+}
+
+#ifndef WIN32
+TEST_F(Sigar, test_sigar_proc_list_get_children) {
+    auto pids = cb::io::mkdtemp("t_pchilds_s");
+    // create some childs and register all of them
+    for (int ii = 0; ii < 3; ++ii) {
+        auto pid = fork();
+        if (pid == 0) {
+            // create a grandchild
+            switch (fork()) {
+            case 0:
+                createPidFileAndWait(pids);
+                // NOT REACHED
+
+            case (pid_t)-1:
+                perror("fork()");
+                exit(EXIT_FAILURE);
+            default:
+                    ;
+            }
+
+            // child
+            createPidFileAndWait(pids);
+            // Not reached
+        } else if (pid == -1) {
+            perror("fork()");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Wait until they're all running
+    std::vector<std::string> files;
+    while (files.size() < 6) {
+        // check if any processes died!
+        files = cb::io::findFilesContaining(pids, "pid");
+    }
+
+    // We've got all of the processes running.. now lets check if sigar gives me
+    // 6 processes
+    sigar_proc_list_t proc_list;
+    const auto ret = sigar_proc_list_get_children(instance, getpid(), &proc_list);
+    ASSERT_EQ(SIGAR_OK, ret)
+            << "sigar_proc_list_get_children: " << sigar_strerror(instance, ret);
+
+    EXPECT_EQ(6, proc_list.number) << "I expected to get 6 childs";
+    sigar_proc_list_destroy(instance, &proc_list);
+    cb::io::rmrf(pids);
+}
+#endif

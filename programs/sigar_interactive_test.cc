c1152685d1164fd5262598a3d9252a375eef0beb
Trond Norbye <trond.norbye@gmail.com>
2023-05-05 11:35:39 +0200

    Add option to generate a snapshot from the test program
    This makes testing easier as one don't need an explicit start
    of sigar_port to get the information.
    
    Change-Id: Ib803745d59de6b400296380146114ebbc311bdaf
    Reviewed-on: https://review.couchbase.org/c/sigar/+/190562
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Vesko Karaganev <vesko.karaganev@couchbase.com>

--- programs/sigar_interactive_test.cc
+++ programs/sigar_interactive_test.cc
@@ -8,6 +8,9 @@
  *   the file licenses/APL2.txt.
  */
 
+#include "sigar_port.h"
+
+#include <platform/split_string.h>
 #include <array>
 #include <chrono>
 #include <cstring>
@@ -21,26 +24,101 @@
 #include <unistd.h>
 #endif
 
-using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+std::vector<char> vector;
+
+static void resize(std::vector<std::string_view>& arguments) {
+    if (arguments.size() != 2) {
+        fprintf(stderr, "Usage: resize <size>\n");
+        return;
+    }
+    std::string argument{arguments[1]};
+    auto val = std::stoul(argument);
+    if (argument.find_first_of("kK") != std::string::npos) {
+        val *= 1024;
+    } else if (argument.find_first_of("mM") != std::string::npos) {
+        val *= 1024 * 1024;
+    } else if (argument.find_first_of("gG") != std::string::npos) {
+        val *= 1024 * 1024 * 1024;
+    }
+    vector.resize(val);
+    vector.shrink_to_fit();
+    std::fill(vector.begin(), vector.end(), 'a');
+}
+
+static void loop(std::vector<std::string_view>& arguments) {
+    if (arguments.size() != 2) {
+        fprintf(stderr, "Usage: loop <seconds>\n");
+        return;
+    }
+    std::string argument{arguments[1]};
+
+    auto timeout = std::chrono::steady_clock::now() +
+                   std::chrono::seconds{stoi(argument)};
+    while (std::chrono::steady_clock::now() < timeout) {
+        // do nothing
+    }
+}
+
+static void readFile(std::vector<std::string_view>& arguments) {
+    if (arguments.size() != 2) {
+        fprintf(stderr, "Usage: read <filename>\n");
+        return;
+    }
+    std::string argument{arguments[1]};
+    if (vector.empty()) {
+        fprintf(stderr,
+                "Resize the vector to the size of chunks you want "
+                "to read\n");
+        return;
+    }
+    FILE* fp = fopen(argument.c_str(), "r");
+    if (fp) {
+        while (!ferror(fp) && !feof(fp)) {
+            (void)fread(vector.data(), 1, vector.size(), fp);
+        }
+        fclose(fp);
+    } else {
+        perror("Failed to open file");
+    }
+}
+
+static void snapshot(std::vector<std::string_view>& arguments) {
+    if (arguments.size() != 1) {
+        fprintf(stderr, "Usage: snapshot\n");
+        return;
+    }
+    sigar_port_snapshot(getpid());
+    fprintf(stdout, "\n");
+}
+
+static void help() {
+    fprintf(stdout,
+            R"(Commands:
+    resize <number> - Resize buffer to the provided number
+    loop <seconds>  - Constantly read the clock in a loop the next
+                      provided number of seconds
+    read <filename> - Read the named file (and discard the output)
+    snapshot        - Dump the sigar_port snapshot
+    help            - This help text
+    quit            - End program
+
+)");
+}
 
 /**
  * sigar_test is a program one may use to help look at the statistics
  * reported from sigar.
  */
 int main() {
-    fprintf(stdout,
-            R"(Running as process %u
+    fprintf(stdout, "Running as process %u\n\n", getpid());
+    help();
 
-Commands:
-    resize <number> - Resize buffer to the provided number
-    loop <seconds>  - Constantly read the clock in a loop the next
-                      provided number of seconds
-    read <filename> - Read the named file (and discard the output)
-
-)",
-            getpid());
-
-    std::vector<char> vector;
+    sigar_port::input = stdin;
+    sigar_port::output = stdout;
+    sigar_port::error = stderr;
+    sigar_port::indentation = 2;
 
     while (!feof(stdin)) {
         fprintf(stdout, "Enter command> ");
@@ -57,50 +135,23 @@ Commands:
         if (index != std::string::npos) {
             data.resize(index);
         }
-
-        index = data.find(' ');
-        if (index == std::string::npos) {
-            fprintf(stderr, "usage: command value\n");
+        auto arguments = cb::string::split(data);
+        if (arguments.empty()) {
+            help();
             continue;
         }
-        auto command = data.substr(0, index);
-        auto argument = data.substr(index + 1);
 
         try {
-            if (command == "resize"s) {
-                auto val = std::stoul(argument);
-                if (argument.find_first_of("kK") != std::string::npos) {
-                    val *= 1024;
-                } else if (argument.find_first_of("mM") != std::string::npos) {
-                    val *= 1024 * 1024;
-                } else if (argument.find_first_of("gG") != std::string::npos) {
-                    val *= 1024 * 1024 * 1024;
-                }
-                vector.resize(val);
-                vector.shrink_to_fit();
-                std::fill(vector.begin(), vector.end(), 'a');
-            } else if (command == "loop"s) {
-                auto timeout = std::chrono::steady_clock::now() +
-                               std::chrono::seconds{stoi(argument)};
-                while (std::chrono::steady_clock::now() < timeout) {
-                    // do nothing
-                }
-            } else if (command == "read"s) {
-                if (vector.empty()) {
-                    fprintf(stderr,
-                            "Resize the vector to the size of chunks you want "
-                            "to read\n");
-                } else {
-                    FILE* fp = fopen(argument.c_str(), "r");
-                    if (fp) {
-                        while (!ferror(fp) && !feof(fp)) {
-                            (void)fread(vector.data(), 1, vector.size(), fp);
-                        }
-                        fclose(fp);
-                    } else {
-                        perror("Failed to open file");
-                    }
-                }
+            if (arguments.front() == "resize"sv) {
+                resize(arguments);
+            } else if (arguments.front() == "loop"sv) {
+                loop(arguments);
+            } else if (arguments.front() == "read"sv) {
+                readFile(arguments);
+            } else if (arguments.front() == "snapshot"sv) {
+                snapshot(arguments);
+            } else if (arguments.front() == "help"sv) {
+                help();
             } else {
                 fprintf(stderr, "Unknown command\n");
             }
